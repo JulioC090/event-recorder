a30007f02a06851223a3cb0f4564dbb451e093ae
JulioC090 <juliocziviani@gmail.com>
2025-04-19 23:37:29 -0300

    feat(client): add development-only page route visible only in dev environment

--- projects/client/src/main.tsx
+++ projects/client/src/main.tsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Navigate, Route, Routes } from 'react-router';
 import './index.css';
 import Home from './pages/Home.tsx';
 import Recorder from './pages/Recorder.tsx';
+import Test from './pages/Test.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -11,6 +12,11 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/recorder/:url" element={<Recorder />} />
+
+        {import.meta.env.MODE === 'development' && (
+          <Route path="/test" element={<Test />} />
+        )}
+
         <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </BrowserRouter>
