5505c97547dd2487ce1b6cdd7c6f43882d4d3b30
JulioC090 <juliocziviani@gmail.com>
2025-05-03 00:34:17 -0300

    refactor(server): update URL resolution to use BASE_URL in HTMLProxyHandle

--- projects/server/src/core/proxy/ProxyHandles/HTMLProxyHandle.ts
+++ projects/server/src/core/proxy/ProxyHandles/HTMLProxyHandle.ts
@@ -1,4 +1,4 @@
-import { BASE_URL, HOST, PORT } from '@/config/serverConfig';
+import { BASE_URL } from '@/config/serverConfig';
 import { IProxyHandleResult } from '@/types/IProxyHandle';
 
 const injectClientScripts = () => `
@@ -34,7 +34,7 @@ export default async function HTMLProxyHandle(
     // Rewrites absolute URLs (starting with http/https) in src or href attributes
     // to go through your proxy server.
     .replace(/(src|href)=["'](http[^"']*)["']/g, (match, attr, path) => {
-      return `${attr}="http://${HOST}:${PORT}/${path}"`;
+      return `${attr}="${BASE_URL}/${path}"`;
     })
     // Rewrites *root-relative* paths (e.g., /images/logo.png) in src, href, or action attributes
     // to be absolute URLs based on the original `targetURL`, and prepends the proxy prefix.
@@ -42,7 +42,7 @@ export default async function HTMLProxyHandle(
       /(<[^>]+\s(?:src|href|action)=["'])(\/[^"'>]+)/gi,
       (_, prefix, path) => {
         const fullUrl = new URL(path, targetURL).toString();
-        return `${prefix}http://${HOST}:${PORT}/${fullUrl}`;
+        return `${prefix}${BASE_URL}/${fullUrl}`;
       },
     )
     // Rewrites *relative* paths (e.g., images/logo.png) that don’t start with /, http, data:, etc.
@@ -51,7 +51,7 @@ export default async function HTMLProxyHandle(
       /(<[^>]+\s(?:src|href|action)=["'])(?!https?:\/\/|\/\/|data:|\/)([^"'>]+)/gi,
       (_, prefix, path) => {
         const fullUrl = new URL(path, targetURL).toString();
-        return `${prefix}http://${HOST}:${PORT}/${fullUrl}`;
+        return `${prefix}${BASE_URL}/${fullUrl}`;
       },
     )
     // Injects a <base> tag inside the <head> to help resolve relative paths correctly in the browser.
