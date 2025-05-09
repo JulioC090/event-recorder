c9a629be29e1eb4f8d9053839dd9546d25dbddc5
JulioC090 <juliocziviani@gmail.com>
2025-05-09 19:36:47 -0300

    chore(server): update build process and add server entry point

---
+++ projects/server/tsup.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+  entry: ['src/index.ts', 'src/server.ts'],
+  clean: true,
+  tsconfig: 'tsconfig.json',
+  treeshake: true,
+  minify: true,
+  shims: true,
+  dts: true,
+});
